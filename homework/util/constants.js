38360c32b1338249c7f29259871cc540a636eed4
Ghukasyan19 <amalyaghukasyan82@gmail.com>
2022-05-15 16:14:27 +0400

    homework

--- homework/util/constants.js
+++ homework/util/constants.js
@@ -3,7 +3,13 @@ const constants = {
     ENCODING: 'UTF-8'
 }
 
+
+
 object.freeze(constants);
+
+// console.log(constants.ENCODING);
+// constants.ENCODING = 'gfjyfgyju';
+// console.log(constants.ENCODING);
 
 
 module.exports = constants;
--- homework/util/util.js
+++ homework/util/util.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 
+// const  array = ['Ani', 'Minas', 'Gayane', 'Lysine'];
+//fs.appendFileSync('./homework/file.txt', 'simple text', 'utf-8');
+
 function  writeToFile(data, filePath, encoding = 'utf-8'){
     for (let i = 0; i < data.length; i++) {
     fs.appendFileSync(filePath, data[i] + ' ', encoding);
     }
 }
+// writeToFile(array, './homework/file.txt');
+
 
 function readFromFile(filePath, encoding = 'utf-8'){
 try {
@@ -21,6 +26,9 @@ function toArray(str) {
         return str.split(' ').filter(Boolean).sort();
     }
 }
+
+// console.log(readFromFile('./homework/file.txt'));
+// console.log(toArray(readFromFile('./homework/file.txt')));
 
 module.exports = {
     writeToFile,
